a0a9be1c786f0aa4a6a6b52e2c304ad132a61aae
Mark Steve Samson <hello@marksteve.com>
2014-02-19 23:10:12 +0800

    Authentication

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 
   "github.com/drone/routes"
   "github.com/dustin/randbo"
+  "github.com/gorilla/context"
   "github.com/hoisie/redis"
+  "github.com/memcachier/bcrypt"
 )
 
 var redisAddr string
@@ -43,9 +45,40 @@ type Error struct {
   Message string `json:"message"`
 }
 
+type Creds struct {
+  Name     string `json:"name"`
+  Password string `json:"password"`
+}
+
+type User struct {
+  Name  string `json:"name"`
+  Token string `json:"token"`
+  Hash  string `json:"-"`
+}
+
 func jsonError(w http.ResponseWriter, message string, code int) {
   http.Error(w, "", code)
   routes.ServeJson(w, &Error{message})
+}
+
+func getJson(v interface{}, w http.ResponseWriter, r *http.Request) {
+  data, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    jsonError(w, "Invalid body.", http.StatusBadRequest)
+    return
+  }
+  err = json.Unmarshal(data, &v)
+  if err != nil {
+    jsonError(w, "Invalid JSON.", http.StatusBadRequest)
+    return
+  }
+}
+
+func genRandString(n int) string {
+  buf := make([]byte, n)
+  randbo.New().Read(buf)
+  id := base64.URLEncoding.EncodeToString(buf)
+  return id
 }
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
@@ -65,61 +98,136 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
+  withUser(w, r)
+  author, ok := context.GetOk(r, "user")
+  if !ok {
+    jsonError(w, "You need to login to post.", http.StatusUnauthorized)
+    return
+  }
   defer r.Body.Close()
-  buf := make([]byte, 9)
-  randbo.New().Read(buf)
-  id := base64.URLEncoding.EncodeToString(buf)
+  id := genRandString(9)
   res := r.URL.Query().Get(":res")
-  post := Post{
-    Id:        id,
-    Timestamp: time.Now().UnixNano(),
-  }
-  data, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    jsonError(w, "Invalid body.", http.StatusBadRequest)
-    return
-  }
-  err = json.Unmarshal(data, &post)
-  if err != nil {
-    jsonError(w, "Invalid JSON.", http.StatusBadRequest)
-    return
-  }
+  post := Post{}
+  getJson(&post, w, r)
+  post.Id = id
   if post.Re != "" {
     if ok, _ := redisCli.Exists(key(res, post.Re)); !ok {
       jsonError(w, "Invalid re.", http.StatusBadRequest)
       return
     }
   }
+  post.Author = author.(User).Name
+  post.Timestamp = time.Now().UnixNano()
   post.Votes = 0
   redisCli.Hmset(key(res, id), post)
   redisCli.Sadd(key(res), []byte(id))
   routes.ServeJson(w, post)
 }
 
-func checkPost(w http.ResponseWriter, r *http.Request) {
+func withPost(w http.ResponseWriter, r *http.Request) {
   params := r.URL.Query()
   res := params.Get(":res")
   id := params.Get(":id")
-  if ok, _ := redisCli.Exists(key(res, id)); !ok {
+  var p Post
+  err := redisCli.Hgetall(key(res, id), &p)
+  if err != nil {
     jsonError(w, "Missing post", http.StatusNotFound)
     return
   }
+  context.Set(r, "post", p)
+}
+
+func withUser(w http.ResponseWriter, r *http.Request) {
+  auth := r.Header.Get("Authorization")
+  if auth == "" {
+    return
+  }
+  authSplit := strings.SplitN(auth, " ", 2)
+  if authSplit[0] != "token" {
+    return
+  }
+  token := authSplit[1]
+  name, err := redisCli.Get(key("tokens", token))
+  if len(name) < 1 || err != nil {
+    return
+  }
+  // FIXME: Cache
+  var u User
+  err = redisCli.Hgetall(key("users", string(name)), &u)
+  if err != nil {
+    return
+  }
+  context.Set(r, "user", u)
+}
+
+func setToken(name string) string {
+  token := genRandString(27)
+  redisCli.Setex(key("tokens", token), 60*60*24*7, []byte(name))
+  return token
 }
 
 func updatePost(w http.ResponseWriter, r *http.Request) {
+  context.Clear(r)
 }
 
 func delPost(w http.ResponseWriter, r *http.Request) {
+  context.Clear(r)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+  var c Creds
+  getJson(&c, w, r)
+  if ok, _ := redisCli.Exists(key("users", c.Name)); ok {
+    jsonError(w, "Invalid name.", http.StatusBadRequest)
+    return
+  }
+  salt, err := bcrypt.GenSalt(10)
+  if err != nil {
+    jsonError(w, "Oops.", http.StatusBadRequest)
+    return
+  }
+  hash, err := bcrypt.Crypt(c.Password, salt)
+  if err != nil {
+    jsonError(w, "Oops.", http.StatusBadRequest)
+    return
+  }
+  u := User{
+    Name: c.Name,
+    Hash: hash,
+  }
+  u.Token = setToken(c.Name)
+  redisCli.Hmset(key("users", c.Name), u)
+  routes.ServeJson(w, u)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+  var c Creds
+  getJson(&c, w, r)
+  var u User
+  err := redisCli.Hgetall(key("users", c.Name), &u)
+  if err == nil {
+    jsonError(w, "Invalid login.", http.StatusUnauthorized)
+    return
+  }
+  if ok, err := bcrypt.Verify(c.Password, u.Hash); !ok || err != nil {
+    jsonError(w, "Invalid login.", http.StatusUnauthorized)
+    return
+  }
+  u.Token = setToken(c.Name)
+  routes.ServeJson(w, u)
 }
 
 func main() {
   redisCli.Addr = redisAddr
   mux := routes.New()
+  mux.Post("/register", register)
+  mux.Post("/login", login)
   mux.Get("/:res", getPosts)
   mux.Post("/:res", createPost)
   mux.Put("/:res/:id", updatePost)
   mux.Del("/:res/:id", delPost)
-  mux.FilterParam("id", checkPost)
+  mux.FilterParam("id", withPost)
+  mux.FilterParam("id", withUser)
   log.Fatal(http.ListenAndServe(port, mux))
 }
 
